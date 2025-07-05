a7d7f493bb3faaf347b8d3dd8f0c062022b2ad21
Aditya Yadav <Adityay1109@gmail.com>
2025-07-05 14:13:12 +0530

    .

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ import User from "./models/User.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://www.bhavanamatrimony.com', // Your domain
+  credentials: true
+}));
+
 app.use(express.json());
 
 // MongoDB Connection
