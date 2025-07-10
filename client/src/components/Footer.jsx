e419dd7386eeaea84b2c5c987f517548b74a1933
Aditya Yadav <Adityay1109@gmail.com>
2025-07-10 14:49:10 +0530

    .

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,13 @@
 import { Link } from "react-router-dom";
-import { FaInstagram, FaXTwitter, FaFacebookF, FaYoutube, FaTumblr, FaMedium } from "react-icons/fa6";
+import {
+  FaInstagram,
+  FaXTwitter,
+  FaFacebookF,
+  FaYoutube,
+  FaTumblr,
+  FaMedium,
+  FaWhatsapp,
+} from "react-icons/fa6";
 import { SiBlogger } from "react-icons/si"; // Blogger icon from simple-icons
 
 const Footer = () => {
@@ -39,27 +47,69 @@ const Footer = () => {
 
       {/* Social Media Icons */}
       <div className="flex justify-center gap-4 mb-4 text-xl text-gray-600 flex-wrap">
-        <a href="https://www.instagram.com/bhavanamatrimony?igsh=N3owcWppeWl2b243" target="_blank" rel="noopener noreferrer" className="hover:text-red-500">
+        <a
+          href="https://www.instagram.com/bhavanamatrimony?igsh=N3owcWppeWl2b243"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-red-500"
+        >
           <FaInstagram />
         </a>
-        <a href="https://x.com/BhavanaMatri9?t=kfFutu1A1DXaCDLciIAXpw&s=09" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
+        <a
+          href="https://x.com/BhavanaMatri9?t=kfFutu1A1DXaCDLciIAXpw&s=09"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-blue-500"
+        >
           <FaXTwitter />
         </a>
-        <a href="https://www.facebook.com/share/1B1JrsibiU/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-700">
+        <a
+          href="https://www.facebook.com/share/1B1JrsibiU/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-blue-700"
+        >
           <FaFacebookF />
         </a>
-        <a href="https://www.youtube.com/@BhavanaMatrimony" target="_blank" rel="noopener noreferrer" className="hover:text-red-600">
+        <a
+          href="https://www.youtube.com/@BhavanaMatrimony"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-red-600"
+        >
           <FaYoutube />
         </a>
-        <a href="https://www.tumblr.com/bhavanamatrimony?source=share
-        " target="_blank" rel="noopener noreferrer" className="hover:text-blue-400">
+        <a
+          href="https://www.tumblr.com/bhavanamatrimony?source=share"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-blue-400"
+        >
           <FaTumblr />
         </a>
-        <a href="https://bhavanamat.blogspot.com/2025/07/marriage.html" target="_blank" rel="noopener noreferrer" className="hover:text-orange-600">
+        <a
+          href="https://bhavanamat.blogspot.com/2025/07/marriage.html"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-orange-600"
+        >
           <SiBlogger />
         </a>
-        <a href="https://medium.com/@matrimonybhavana/आधुनिक-live-in-रिलेशनशिप-व-पारंपरिक-विवाह-एक-तुलनात्मक-दृष्टिकोन-56c1b94b7979" target="_blank" rel="noopener noreferrer" className="hover:text-black">
+        <a
+          href="https://medium.com/@matrimonybhavana/आधुनिक-live-in-रिलेशनशिप-व-पारंपरिक-विवाह-एक-तुलनात्मक-दृष्टिकोन-56c1b94b7979"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-black"
+        >
           <FaMedium />
+        </a>
+        <a
+          href="https://whatsapp.com/channel/0029Vb5m22o59PwaieoXOA2M" // Replace with your actual WhatsApp number (in international format)
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-green-500"
+        >
+          <FaWhatsapp />
         </a>
       </div>
 
